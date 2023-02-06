cf4e329388aeb99b8974f804886fcdc93e75e2b7
M. Hasan Celik <muhammedhasancelik@gmail.com>
2023-02-06 00:06:14 -0800

    copilot key mapping

--- lua/config/plugins/copilot.lua
+++ lua/config/plugins/copilot.lua
@@ -2,4 +2,8 @@ local M = {
   "github/copilot.vim",
 }
 
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-.>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.g.copilot_assume_mapped = true
+
 return M
